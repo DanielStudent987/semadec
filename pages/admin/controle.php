a3f8ed675ddd891b799c7adf646e60aa8a16599c
Daniel R. Gomes <dr987100@gmail.com>
2022-10-20 09:08:40 -0300

    #6 Cadastro de provas atualizado contra sql-injection

--- pages/admin/controle.php
+++ pages/admin/controle.php
@@ -1,5 +1,5 @@
 <?php
-    include('../conexao.php');
+    
     include('protecao.php');
 
     if (isset($_GET['p1'])) {
@@ -8,6 +8,8 @@
 
             //DELETAR EQUIPE
             if (isset($_GET['deletarequipe'])) {
+                include('../conexao.php');
+
                 $sql_query = $mysqli->query("SELECT idUsuario  from equipe e, usuario u where e.idEquipe=$_GET[deletarequipe] and 
                 u.idUsuario=e.Usuario_idUsuario");
                 $sql = $sql_query->fetch_assoc();
@@ -23,9 +25,12 @@
                         }
                     } 
                 }
+                $mysqli->close();
 
             //DELETAR MEMBRO
             } else if (isset($_GET['deletarmembro'])) {
+                include('../conexao.php');
+
                 $id_deletar = intval($_GET['deletarmembro']);
                 $sql_lista_membro_query = $mysqli->query("SELECT * from membros where idMembro = '$id_deletar'") or die($mysqli->error);
                 $dados = $sql_lista_membro_query->fetch_assoc();
@@ -44,9 +49,12 @@
                         }
                     }
                 }
-                
+                $mysqli->close();
             //CADASTRAR PROVAS
             } else if (isset($_GET['cadastrarprova'])) {
+                include('../conexao.php');
+                
+
                 if ($_GET['cadastrarprova']==1) {
                     $nome = $_GET['nome_prova'];
                     $local = $_GET['local_prova'];
@@ -66,7 +74,43 @@
 
                     //INSERINDO OS DADOS NO BANCO DE DADOS
                     if ($i==0) {
-                        if ($mysqli->query("INSERT INTO provas (nome, local, participantes, pontuacao_idpontuacao)  VALUES ('$nome', '$local', '$part', '$idgrupo')")) {
+                        
+
+                        if ($stmt = $mysqli->prepare("INSERT INTO provas (nome, local, participantes, pontuacao_idpontuacao)  VALUES (?, ?, ?, ?)")) {
+                            //vincular valores as interrogacoes (?)
+                            mysqli_stmt_bind_param($stmt,'ssii',$nome, $local, $part, $idgrupo);
+                            //efetiva e executa a SQL no banco, i.e., insere
+                            $status = mysqli_stmt_execute($stmt);
+                            if ($status === false) {
+                                trigger_error($stmt->error, E_USER_ERROR);
+                              }
+                            
+                            $sql_query = $mysqli->query("SELECT e.idEquipe, p.idProva from equipe e, provas p where p.nome = '$nome' order by e.idEquipe");
+
+                            while ($dados=$sql_query->fetch_assoc()) {
+                                if ($stmt = $mysqli->prepare("INSERT into conquistas (Provas_idProva, Equipe_idEquipe, classificacao, nota) values (?, ?, ?, ?)")) {
+                                    //vincular valores as interrogacoes (?)
+                                    $zero = 0;
+                                    $string = '';
+                                    mysqli_stmt_bind_param($stmt,'iisi',$dados['idProva'], $dados['idEquipe'], $string, $zero);
+                                    //efetiva e executa a SQL no banco, i.e., insere
+                                    $status = mysqli_stmt_execute($stmt);
+                                    if ($status === false) {
+                                        trigger_error($stmt->error, E_USER_ERROR);
+                                      }
+                                }
+
+                                /*if ($mysqli->query("INSERT into conquistas (Provas_idProva, Equipe_idEquipe, classificacao, nota) values ('$dados[idProva]', '$dados[idEquipe]', '', 0)")) {
+                                    header("location:gerenciarprovas.php");
+                                }*/
+                            }
+                            
+                            //fecha o statement
+                            mysqli_stmt_close($stmt); 
+                            header("location:gerenciarprovas.php");
+                        }
+
+                        /*if ($mysqli->query("INSERT INTO provas (nome, local, participantes, pontuacao_idpontuacao)  VALUES ('$nome', '$local', '$part', '$idgrupo')")) {
                             $sql_query = $mysqli->query("SELECT e.idEquipe, p.idProva from equipe e, provas p where p.nome = '$nome' order by e.idEquipe");
 
                             //ESTRUTURA QUE FAZ UMA INSERSAO EM CONQUISTA POR EQUIPE CADASTRADA
@@ -76,7 +120,7 @@
                                 }
                            }
                             
-                        }
+                        }*/
                     }
 
                 } else {
@@ -86,13 +130,18 @@
             //DELETAR PROVA
             } else if (isset($_GET['deletarprova'])) {
                 if ($_GET['deletar']==2) {
+                    include('../conexao.php');
+
                     $idP = $_GET['deletarprova'];
                     
                     if ($mysqli->query("DELETE from conquistas where Provas_idProva='$idP'") or die("Erro ao deletar prova de conquistas".$mysqli->error)) {
                         if  ($mysqli->query("DELETE from provas where idProva='$idP'") or die("Erro ao deletar prova de provas".$mysqli->error)) {
                             header("location:gerenciarprovas.php");
+
                         }
+
                     }
+                    $mysqli->close();
 
                 } else {
                     die("Acesso negado");
